a2a3195a148eaed24ec53d8706b80005909cab91
Raul Sanchez Fernandez <raul.sanchez@unit4.com>
2023-11-14 15:12:59 +0100

    Added vulnerability

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+import sys
+from flask import Flask, abort, make_response, redirect, render_template, render_template_string, request
 import base64
 app = Flask(__name__)
 
@@ -6,22 +7,62 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
-@app.route("/login")
-def login():
-    return render_template("login.html")
+@app.get("/logout")
+def logout():
+    response = redirect("/login")
+    response.set_cookie("login_cookie", "")
+    return response
 
-@app.route("/admin")
+@app.get("/login")
+def login():
+    login_cookie = request.cookies.get('login_cookie')
+    
+    if (login_cookie is None):
+        return render_template("login.html")
+    
+    try:
+        name, role = decode_cookie(login_cookie)
+    except:
+        return render_template("login.html", logged_in = False)
+    
+    return render_template("login.html", logged_in = True, username = name, role = role)
+
+@app.post("/login")
+def post_login():
+    username = request.form.get('username')
+    if username is None: abort(400)
+    password = request.form.get('password')
+    if password is None: abort(400)
+
+    login_cookie = base64.b64encode(bytes(f"{username};regular_user", 'utf-8')).decode('utf8')
+    response = redirect("/login")
+    response.set_cookie("login_cookie", login_cookie)
+    
+    return response
+
+@app.get("/admin")
 def admin():
     login_cookie = request.cookies.get('login_cookie')
 
-    if (login_cookie is not None):
-        decoded_cookie = base64.b64decode(login_cookie)
-        name, role = decoded_cookie.split(";")
-        is_admin = role == "admin"
-    else:
-        is_admin = False
+    if (login_cookie is None):
+        return render_template("admin.html", is_admin = False)
+        
+    try:
+        name, role = decode_cookie(login_cookie)
+    except:
+        return render_template("admin.html", is_admin = False)
+    
+    is_admin = role == "admin"
 
-    return render_template("admin.html", is_admin = is_admin)
+    f = open(f'{sys.argv[0]}/../templates/admin.html')
+    admin_template = f.read().replace('admin_name', name)
+
+    return render_template_string(admin_template, is_admin = is_admin)
+
+def decode_cookie(login_cookie):
+    decoded_cookie = base64.b64decode(login_cookie).decode('utf8')
+    name, role = decoded_cookie.split(";")
+    return name, role 
 
 if __name__ == "__main__":
     # Only for debugging while developing
