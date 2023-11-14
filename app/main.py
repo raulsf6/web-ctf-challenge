97faf51cc74a868da7f6f087e61ab4fb730499c0
Raul Sanchez Fernandez <raul.sanchez@unit4.com>
2023-11-14 11:16:28 +0100

    Added base template

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World from Flask"
+    return render_template("base.html")
 
 if __name__ == "__main__":
     # Only for debugging while developing
