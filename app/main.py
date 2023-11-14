afc360d20571fc5e705a5d04e4676cbd077ba31e
Raul Sanchez Fernandez <raul.sanchez@unit4.com>
2023-11-14 11:32:49 +0100

    Splitted templates

--- app/main.py
+++ app/main.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return render_template("base.html")
+def home():
+    return render_template("home.html")
 
 if __name__ == "__main__":
     # Only for debugging while developing
